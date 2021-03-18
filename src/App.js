d6a2a0cfe26db87691a5eba7adc1c63558064031
John Souza <souzajohnlee@gmail.com>
2021-03-17 19:44:21 -0700

    wrap generator in card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import QRCode from "qrcode";
 import { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, Card } from "react-bootstrap";
 
 function App() {
   const [showCode, setShowCode] = useState(false);
@@ -24,23 +24,26 @@ function App() {
       <div className="background-picture"></div>
       <div className="background-overlay"></div>
       <Container className="my-5">
-        <h1 className="text-center my-2" style={{color: "white"}}>QR Code-ify</h1>
-        <h3 className="my-3" style={{color: "white"}}>Enter your Website</h3>
+        <Card className="card h-75 p-4">
+
+        <h1 className="text-center my-2" >QR Code-ify</h1>
+        <h3 className="my-3">Enter your Website</h3>
         <Form className="d-flex justify-content-center flex-column">
           <input
             className="form-control"
             id="qr-value"
             placeholder="enter your website"
-          ></input>
+            ></input>
           <Button
             className="btn btn-primary my-2 float-center"
             type="submit"
             onClick={(e) => handleClick(e)}
-          >
+            >
             QRCode-ify
           </Button>
         </Form>
-        <canvas id="canvas"></canvas>
+        <canvas id="canvas" style={!showCode ? {display: "none"} : null}></canvas>
+        </Card>
       </Container>
     </div>
   );
